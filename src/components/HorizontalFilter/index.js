66c000c97e564e8f3c64f62b7c4570a9cbcd815c
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-03-14 23:39:31 +0900

    ✨ :: add roadingButton Component

--- src/components/HorizontalFilter/index.js
+++ src/components/HorizontalFilter/index.js
@@ -1,10 +1,25 @@
-export default function HorizontalFilter() {
+export default function HorizontalFilter(props) {
     return(
         <div className="row align-items-center">
             <div className="col text-center">
                 <div className="new_arrivals_sorting">
                     <ul className="arrivals_grid_sorting">
-                        
+                        <li className="grid_sorting_button button d-flex flex-column jutify-content-center align-items-center active is-checked" 
+                        data-filter="*">
+                            all
+                        </li>
+                        <li className="grid_sorting_button button d-flex flex-column jutify-content-center align-items-center" 
+                        data-filter=".women">
+                            women's
+                        </li>
+                        <li className="grid_sorting_button button d-flex flex-column jutify-content-center align-items-center" 
+                        data-filter=".accessories">
+                            accessories
+                        </li>
+                        <li className="grid_sorting_button button d-flex flex-column jutify-content-center align-items-center" 
+                        data-filter=".men">
+                            men's
+                        </li>
                     </ul>
                 </div>
             </div>
---
+++ src/components/LoadingButton/index.js
@@ -0,0 +1,35 @@
+import classNames from "classnames";
+import propTypes from "prop-types"
+
+export default function LoadingButton(props) {
+    return(
+        <button
+            type={props.type}
+            className={classNames(
+                "btn btn-wide",
+                props.className,
+                {
+                    "btn-wait": props.loading
+                },
+                {disabled : props.loading}
+            )}
+            style={props.style}
+            onClick={props.onClick}
+        >
+            {props.loading ? "Please wait..." : <span>{props.children}</span>}
+        </button>
+    )
+}
+
+LoadingButton.propTypes = {
+    className: propTypes.string,
+    loading: propTypes.bool,
+    onClick: propTypes.func,
+    type: propTypes.string
+}
+
+LoadingButton.defaultProps = {
+    className: "btn-primary btn-7",
+    loading: false,
+    type: "button"
+};
